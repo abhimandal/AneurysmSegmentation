364a50b8de2b6a3cb2250f89b08dabc8e8613793
abhimandal <abhilashmandal911@gmail.com>
2021-06-08 12:15:16 +0530

    Training

---
+++ aneurysm_segmentation3d/scripts/modelling/model.py
@@ -0,0 +1,67 @@
+import os, sys
+import torch
+import pyvista as pv
+import pandas as pd
+import numpy as np
+
+sys.path.append(os.getcwd())
+
+from torch_points3d.applications.kpconv import KPConv
+from torch_points3d.core.common_modules.base_modules import (
+    MultiHeadClassifier,
+)
+
+
+class PartSegKPConv(torch.nn.Module):
+    def __init__(self, cat_to_seg):
+        super().__init__()
+        self.unet = KPConv(
+            architecture="unet",
+            input_nc=4,
+            num_layers=4,
+            in_grid_size=0.02,
+        )
+        self.classifier = MultiHeadClassifier(
+            self.unet.output_nc, cat_to_seg
+        )
+
+    @property
+    def conv_type(self):
+        """ This is needed by the dataset to infer which batch collate should be used"""
+        return self.unet.conv_type
+
+    def get_batch(self):
+        return self.batch
+
+    def get_output(self):
+        """ This is needed by the tracker to get access to the ouputs of the network"""
+        return self.output
+
+    def get_labels(self):
+        """ Needed by the tracker in order to access ground truth labels"""
+        return self.labels
+
+    def get_current_losses(self):
+        """ Entry point for the tracker to grab the loss """
+        return {"loss_seg": float(self.loss_seg)}
+
+    def forward(self, data):
+        self.labels = data.y
+        self.batch = data.batch
+
+        # Forward through unet and classifier
+        data_features = self.unet(data)
+        self.output = self.classifier(data_features.x, data.category)
+
+        # Set loss for the backward pass
+        self.loss_seg = torch.nn.functional.nll_loss(
+            self.output, self.labels
+        )
+        return self.output
+
+    def get_spatial_ops(self):
+        return self.unet.get_spatial_ops()
+
+    def backward(self):
+        self.loss_seg.backward()
+
--- aneurysm_segmentation3d/scripts/modelling/train.py
+++ aneurysm_segmentation3d/scripts/modelling/train.py
@@ -1,16 +1,50 @@
-# print(
-#     "__file__={0:<35} | __name__={1:<20} | __package__={2:<20}".format(
-#         __file__, __name__, str(__package__)
-#     )
-# )
-
-# from ..data import AneurysmDataset
-# from aneurysm_segmentation3d.scripts.data import AneurysmDataset
 import os, sys
+import torch
+import pyvista as pv
+import pandas as pd
+import numpy as np
 
-# print(os.getcwd())
+sys.path.append(os.getcwd())
 
-# sys.path.append(os.getcwd())
-# from src.data.AneurysmDataset import AneurysmDataset
-from aneurysm_segmentation3d.scripts.data import AneurysmDataset
+from omegaconf import OmegaConf
 from torch.utils.data import Dataset
+from torch_points3d.core.common_modules.base_modules import (
+    MultiHeadClassifier,
+)
+
+# Own Libs
+from aneurysm_segmentation3d.scripts.data import AneurysmDataset
+from aneurysm_segmentation3d.scripts.modelling.model import (
+    PartSegKPConv,
+)
+from aneurysm_segmentation3d.scripts.modelling.trainer import Trainer
+
+CONF_DIR = "D:\\Workspace\\Python\AneurysmSegmentation\\aneurysm_segmentation3d\\scripts\\data\conf\\conf_base.yaml"
+PARTS_TO_SEGMENT = 5
+
+# category_to_seg = {'aneur': np.arange(PARTS_TO_SEGMENT)}
+category_to_seg = {"aneur": [0, 1, 2, 3, 4]}
+
+# Load Config file
+params = OmegaConf.load(CONF_DIR)
+
+params.dataroot = (
+    "D:\\Workspace\\Python\\aneurysm-segmentation\\datasets\\data"
+)
+dataset = AneurysmDataset.AneurysmDataset(params, category_to_seg)
+
+model = PartSegKPConv(dataset.class_to_segments)
+
+NUM_WORKERS = 0
+BATCH_SIZE = 3
+dataset.create_dataloaders(
+    model,
+    batch_size=BATCH_SIZE,
+    num_workers=NUM_WORKERS,
+    shuffle=True,
+    precompute_multi_scale=True,
+)
+
+# sample = next(iter(dataset.train_dataloader))
+trainer = Trainer(model, dataset)
+trainer.fit()
